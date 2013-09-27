15b5342af372716a0aed5e367a1a43dcd5a750a2
Daniel Parnell <me@danielparnell.com>
2013-09-28 09:42:52 +1000

    Initial Commit

---
+++ eversolar/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  eversolar
+//
+//  Created by Daniel Parnell on 28/09/13.
+//  Copyright (c) 2013 Daniel Parnell. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
